8eff7a5bc85b5228b659ce93682e5f3a3fa174a7
MathBernardino <bernardinogomes310707@gmail.com>
2026-06-16 14:41:49 -0300

    FEAT Ajuste no nome das tabelas do models

--- src/models/madeiraModel.js
+++ src/models/madeiraModel.js
@@ -2,20 +2,22 @@ var database = require("../database/config");
 
 function buscarMadeiraPorUsuario(idUsuario) {
 
-  var instrucaoSql = `SELECT * FROM madeira WHERE fk_usuario = ${usuario_id}`;
+  var instrucaoSql = `SELECT * FROM madeira WHERE fk_usuario = ${idUsuario}`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
 
-function cadastrar(idUsuario, descricao) {
+function cadastrar(descricao, idUsuario, durabilidade, estabilidade, exposicao, selagem) {
   
-  var instrucaoSql = `INSERT INTO (descricao, usuario_id) VALUES (${descricao}, ${usuario_id})`;
+  var instrucaoSql = `
+    INSERT INTO madeira (descricao, fk_usuario, durabilidade, estabilidade, exposicao, selagem) 
+    VALUES ('${descricao}', ${idUsuario}, '${durabilidade}', '${estabilidade}', '${exposicao}', '${selagem}')
+  `;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
-
 
 module.exports = {
   buscarMadeiraPorUsuario,
